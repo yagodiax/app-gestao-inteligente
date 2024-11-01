0c4d93db3187310a7df4646f12a7214296e98cf9
yago <100420066+yagodiax@users.noreply.github.com>
2024-11-01 17:25:46 -0300

    db

--- db/gtech.sql
+++ db/gtech.sql
@@ -1,120 +1,44 @@
-CREATE DATABASE  IF NOT EXISTS "gtech" /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
-USE `gtech`;
--- MySQL dump 10.13  Distrib 8.0.38, for Win64 (x86_64)
---
--- Host: gtechdb-gtech.e.aivencloud.com    Database: gtech
--- ------------------------------------------------------
--- Server version	8.0.30
+CREATE DATABASE gtech; 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-SET @MYSQLDUMP_TEMP_LOG_BIN = @@SESSION.SQL_LOG_BIN;
-SET @@SESSION.SQL_LOG_BIN= 0;
+USE gtech;
 
---
--- GTID state at the beginning of the backup 
---
+CREATE TABLE admin (
+  id int NOT NULL AUTO_INCREMENT,
+  usuario varchar(255) NOT NULL,
+  senha varchar(255) NOT NULL,
+  cargo varchar(50) DEFAULT NULL,
+  PRIMARY KEY (id)
+);
 
-SET @@GLOBAL.GTID_PURGED=/*!80000 '+'*/ '2877e5c4-72b4-11ef-9383-060d8bdf4793:1-59,
-3482d9fa-7da6-11ef-92e3-a2862fad6ec5:1-15,
-7b4d3e91-72e8-11ef-b659-f60b2930ec8b:1-15,
-b9b001a4-72fb-11ef-8b8c-0a5dc612d0e9:1-64,
-e1ec208c-72e6-11ef-b96e-02a5e666d8b5:1-15';
+CREATE TABLE gastos (
+  id int NOT NULL AUTO_INCREMENT,
+  servico varchar(255) NOT NULL,
+  data date NOT NULL,
+  valor decimal(10,2) NOT NULL,
+  forma_pagamento varchar(100) NOT NULL,
+  detalhes varchar(255) DEFAULT NULL,
+  categoria varchar(100) NOT NULL,
+  fornecedor varchar(255) NOT NULL,
+  PRIMARY KEY (id)
+);
 
---
--- Table structure for table `admin`
---
+CREATE TABLE vendas (
+  id int NOT NULL AUTO_INCREMENT,
+  loja varchar(255) DEFAULT NULL,
+  servico varchar(255) DEFAULT NULL,
+  data date DEFAULT NULL,
+  valor decimal(10,2) DEFAULT NULL,
+  forma_pagamento varchar(50) DEFAULT NULL,
+  detalhes varchar(255) DEFAULT NULL,
+  PRIMARY KEY (id),
+  KEY fk_servico (servico),
+  CONSTRAINT fk_servico FOREIGN KEY (servico) REFERENCES servicos (nome)
+);
 
-DROP TABLE IF EXISTS `admin`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `admin` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `usuario` varchar(255) NOT NULL,
-  `senha` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `endereco`
---
-
-DROP TABLE IF EXISTS `endereco`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `endereco` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `loja` varchar(255) DEFAULT NULL,
-  `feirao` varchar(255) DEFAULT NULL,
-  `bloco` varchar(10) DEFAULT NULL,
-  `cidade` varchar(255) DEFAULT NULL,
-  `estado` varchar(2) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `loja` (`loja`),
-  CONSTRAINT `endereco_ibfk_1` FOREIGN KEY (`loja`) REFERENCES `lojas` (`loja`) ON DELETE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `lojas`
---
-
-DROP TABLE IF EXISTS `lojas`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `lojas` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `loja` varchar(255) NOT NULL,
-  `email` varchar(255) DEFAULT NULL,
-  `numero` varchar(20) DEFAULT NULL,
-  `cpf_cnpj` varchar(20) DEFAULT NULL,
-  `logo` blob,
-  `tipo_produto` varchar(255) DEFAULT NULL,
-  `link_site` varchar(255) DEFAULT NULL,
-  `link_instagram` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `loja` (`loja`)
-) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `vendas`
---
-
-DROP TABLE IF EXISTS `vendas`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `vendas` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `loja` varchar(255) DEFAULT NULL,
-  `servico` varchar(255) DEFAULT NULL,
-  `data` date DEFAULT NULL,
-  `valor` decimal(10,2) DEFAULT NULL,
-  `forma_pagamento` varchar(50) DEFAULT NULL,
-  `detalhes` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `loja` (`loja`),
-  CONSTRAINT `vendas_ibfk_1` FOREIGN KEY (`loja`) REFERENCES `lojas` (`loja`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-SET @@SESSION.SQL_LOG_BIN = @MYSQLDUMP_TEMP_LOG_BIN;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2024-09-28 14:02:27
+CREATE TABLE servicos (
+  id int NOT NULL AUTO_INCREMENT,
+  nome varchar(255) NOT NULL,
+  valor decimal(10,2) DEFAULT NULL,
+  PRIMARY KEY (id),
+  UNIQUE KEY nome (nome)
+);
